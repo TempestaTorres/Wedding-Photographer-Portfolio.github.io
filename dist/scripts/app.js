77f33510b0fec12a667ef7a5bfae5e5a83f3393c
TempestaTorres <142347458+TempestaTorres@users.noreply.github.com>
2025-07-03 00:37:53 +0300

    section portfolio added

--- dist/scripts/app.js
+++ dist/scripts/app.js
@@ -5,5 +5,18 @@ document.addEventListener("DOMContentLoaded", function(event) {
 
     // Entry point
     hamburgerHandler();
+    // Gallery
+    Fancybox.bind('[data-fancybox="gallery"]', {
+        Thumbs: {
+            type: "modern",
+        },
+        Toolbar: {
+            display: {
+                left: ["infobar"],
+                middle: ["zoomIn", "zoomOut"],
+                right: [ "slideshow", "thumbs","close"],
+            },
+        },
+    });
 
 });
